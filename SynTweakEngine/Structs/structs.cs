71041728ff52d81ac289efb57a51df1ac39ee8f9
Minizbot2012 <mini@zbots.org>
2022-02-02 14:22:59 -0500

    Initial COmmit

---
+++ SynTweakEngine/Structs/structs.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Skyrim.Plugins;
+using Mutagen.Bethesda.Plugins;
+
+namespace SynTweakEngine.Structs
+{
+    public struct ChangeSPELMGEF {
+        public int Position;
+        public float Mag;
+        public int Duration;
+    }
+    public struct AddSPELMGEF {
+        public FormLink<IMagicEffectGetter> Add;
+        public float Mag;
+        public int Area;
+        public int Duration;
+    }
+    public struct SPELTweak {
+        public FormLink<ISpellGetter> Target;
+        public List<ChangeSPELMGEF>? Change;
+        public List<AddSPELMGEF>? Add;
+    }
+    public struct ENCHTweak {
+        public FormLink<IObjectEffectGetter> Target;
+        public List<ChangeSPELMGEF>? Change;
+        public List<AddSPELMGEF>? Add;
+    }
+    public struct MGEFTweak {
+        public FormLink<IMagicEffectGetter> Target;
+        public FormKey? HitShader;
+    }
+    public struct FLSTTweak {
+        public FormLink<IFormListGetter> Target;
+        public List<IFormLinkGetter<ISkyrimMajorRecordGetter>>? Inject;
+    }
+    public class FileData
+    {
+        public List<SPELTweak>? SPEL;
+        public List<ENCHTweak>? ENCH;
+        public List<MGEFTweak>? MGEF;
+        public List<FLSTTweak>? FLST;
+    }
+}
